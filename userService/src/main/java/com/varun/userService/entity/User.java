b4f0247de71b35badf80aa7a2d35373d32e7998f
Varungunda60 <varungunda60@gmail.com>
2024-02-01 11:08:26 -0800

    User Entity added

--- userService/src/main/java/com/varun/userService/entity/User.java
+++ userService/src/main/java/com/varun/userService/entity/User.java
@@ -1,2 +1,25 @@
-package com.varun.userService.entity;public class User {
+package com.varun.userService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String Password;
+
 }
