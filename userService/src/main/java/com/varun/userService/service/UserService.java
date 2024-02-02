46afeedab43c67e85fc6d290083ecc76e6206d10
Varungunda60 <varungunda60@gmail.com>
2024-02-01 22:28:43 -0800

    getUserById endpoint created

--- userService/src/main/java/com/varun/userService/service/UserService.java
+++ userService/src/main/java/com/varun/userService/service/UserService.java
@@ -5,9 +5,12 @@ import com.varun.userService.entity.User;
 import com.varun.userService.mapper.UserMapper;
 import com.varun.userService.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +21,13 @@ public class UserService {
         List<User> users = userRepository.findAll();
         return users.stream().map(UserMapper.INSTANCE::mapUserToUserDto).toList();
 
+    }
+
+    public ResponseEntity<UserDto> getUserById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return new ResponseEntity<>(UserMapper.INSTANCE.mapUserToUserDto(userOptional.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
